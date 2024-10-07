eec5b6cfc8b17fd9311a01e2e4e9d6ad9b210d00
Hassan Jarko <hassan.jarko@yahoo.com>
2024-10-07 16:27:38 +0400

    updated client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import {BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import Register from './pages/register';
-import Login from './pages/Login';
-import Dashboard from './pages/dashboard';
-
-
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Register from "./pages/register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/dashboard";
 
 function App() {
-
   return (
     <Router>
-     <Routes>
-      <Route path='/register' Component={Register} />
-      <Route path='/login' Component={Login}/>
-      <Route path='/dashboard' Component={Dashboard}/>
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/register" Component={Register} />
+        <Route path="/login" Component={Login} />
+        <Route path="/dashboard" Component={Dashboard} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
